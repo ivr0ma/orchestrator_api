424898bc683b9755ea19cbf340bc79d016d5514d
Roman <ivanov@integrant.ru>
2024-12-27 15:00:34 +0300

    add pulling from docker hub

--- build.sh
+++ build.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-echo \"Building Docker images...\"
-docker build -t todo_sqlite:latest ./todo_app
-docker build -t short_url:latest ./shorturl_app
-docker build -t orchestrator_api_interface:latest ./orchestrator_api_interface
-echo \"Build complete.\"
+
+set -e
+
+DOCKER_HUB_USERNAME="ivroma"
+
+function pull_or_build() {
+  local image_name=$1
+  local dockerfile_path=$2
+
+  echo "Attempting to pull $DOCKER_HUB_USERNAME/$image_name:latest from Docker Hub..."
+  if docker pull "$DOCKER_HUB_USERNAME/$image_name:latest"; then
+    echo "Successfully pulled $DOCKER_HUB_USERNAME/$image_name:latest"
+    docker tag "$DOCKER_HUB_USERNAME/$image_name:latest" "$image_name:latest"
+    docker rmi "$DOCKER_HUB_USERNAME/$image_name:latest"
+  else
+    echo "Failed to pull $DOCKER_HUB_USERNAME/$image_name:latest. Building image locally..."
+    docker build -t "$image_name:latest" "$dockerfile_path"
+  fi
+}
+
+echo "Starting image setup..."
+
+# Pull or build todo-sqlite
+pull_or_build "todo_sqlite" "./todo-sqlite"
+
+# Pull or build short_url
+pull_or_build "short_url" "./short_url"
+
+# Pull or build orchestrator_api_interface
+pull_or_build "orchestrator_api_interface" "./orchestrator_api_interface"
+
+echo "Image setup complete."
